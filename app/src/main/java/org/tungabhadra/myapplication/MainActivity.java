a00eba84020716e14f99979b7450906178245b38
bluebottlewize <udithkrishna.nair@research.iiit.ac.in>
2024-10-26 18:24:35 +0530

    working

--- app/src/main/java/org/tungabhadra/myapplication/MainActivity.java
+++ app/src/main/java/org/tungabhadra/myapplication/MainActivity.java
@@ -8,6 +8,9 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.qualcomm.qti.platformvalidator.PlatformValidator;
+import com.qualcomm.qti.platformvalidator.PlatformValidatorUtil;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -20,5 +23,20 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        PlatformValidator pv = new PlatformValidator(PlatformValidatorUtil.Runtime.DSP);
+// To check in general runtime is working use isRuntimeAvailable
+        boolean check = pv.isRuntimeAvailable(getApplication());
+        System.out.println(check);
+// To check Qualcomm (R) Neural Processing SDK runtime is working use runtimeCheck
+        check = pv.runtimeCheck(getApplication());
+        System.out.println(check);
+//To get core version use libVersion api
+        String str = pv.coreVersion(getApplication());
+        System.out.println(str);
+//To get core version use coreVersion api
+        str = pv.coreVersion(getApplication());
+
+        System.out.println(str);
     }
 }
